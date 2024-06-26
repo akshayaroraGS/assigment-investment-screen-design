6b9f11379b0dbccabce5192ad4c08ad140c63e14
Akshay Arora <akshayarora.gs@gmail.com>
2024-06-26 14:50:51 +0530

    added types

--- src/screens/portfolio/hooks/useTotalCalculations.ts
+++ src/screens/portfolio/hooks/useTotalCalculations.ts
@@ -3,21 +3,21 @@ import {PortfolioResponse} from '../types/PortfolioResponse';
 export const useTotalCalculations = (data: PortfolioResponse | null) => {
   const totalInvestmentValue: number =
     data?.data?.userHolding
-      ?.reduce((acc, currentItem) => {
+      ?.reduce((acc: number, currentItem) => {
         return acc + currentItem.quantity * currentItem.avgPrice;
       }, 0)
       .toFixed(1) ?? 0;
 
   const totalInvestmentCurrentValue: number =
     data?.data?.userHolding
-      ?.reduce((acc, currentItem) => {
+      ?.reduce((acc: number, currentItem) => {
         return acc + currentItem.quantity * currentItem.ltp;
       }, 0)
       .toFixed(1) ?? 0;
 
   const totalLastDayValue: number =
     data?.data?.userHolding
-      ?.reduce((acc, currentItem) => {
+      ?.reduce((acc: number, currentItem) => {
         return acc + currentItem.quantity * currentItem.close;
       }, 0)
       .toFixed(1) ?? 0;
